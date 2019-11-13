48d1f14994be0f4899c52c508542dac25809ab50
innazh <innazh1133@gmail.com>
2019-11-12 23:02:15 -0500

    ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,43 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { AchievementsPageComponent } from './achievements-page/achievements-page.component';
+import { FriendsPageComponent } from './friends-page/friends-page.component';
+import { RouterModule } from '@angular/router';
+import { Profile } from 'selenium-webdriver/firefox';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomePageComponent,
+    ProfilePageComponent,
+    AchievementsPageComponent,
+    FriendsPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomePageComponent
+      },
+      {
+        path: 'friends',
+        component: FriendsPageComponent
+      },
+      {
+        path: 'achievements',
+        component: AchievementsPageComponent
+      },
+      {
+        path: 'profile',
+        component: ProfilePageComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
