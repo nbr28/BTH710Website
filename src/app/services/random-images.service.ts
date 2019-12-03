9e4669838e811e8cb0a74fd943fa51153fbf2ffc
rscotchmer <raescotchmer@gmail.com>
2019-12-02 20:10:27 -0500

    removed some extra bits from achievements and tidied up the home page

--- src/app/services/random-images.service.ts
+++ src/app/services/random-images.service.ts
@@ -8,6 +8,20 @@ export class RandomImagesService {
   constructor() { }
 
   getRandomImage(): String {
-    return "https://picsum.photos/id/"+Math.floor(Math.random()*500)+"/64/64";
+    var http = new XMLHttpRequest();
+    var url = "";
+    var urlIsGood = false;
+
+    while (urlIsGood == false) {
+      url = "https://picsum.photos/id/" + Math.floor(Math.random() * 500) + "/64/64";
+      http.open('HEAD', url, false);
+      http.send();
+
+      if (http.status != 404) {
+        urlIsGood = true;
+      }
+    }
+    
+    return url;
   }
 }
