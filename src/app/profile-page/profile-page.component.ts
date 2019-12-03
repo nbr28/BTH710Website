72e4ea5b12a30c65cf12512cbaddb6957def247e
innazh <innazh1133@gmail.com>
2019-12-03 13:44:50 -0500

    profile page yes

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AchievementManagerService } from '../services/achievement-manager.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,8 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent implements OnInit {
+  pinnedAchievements: any[];
+  allPinnedAchievements: any[];
+  portalAchievements: any[];
+  haloAchievements: any[];
+  
+  constructor(private achievementService: AchievementManagerService) { 
+    this.achievementService.getAllAchievements();
+    this.allPinnedAchievements = this.achievementService.getPinnedAchievements();
+    
+    var l=0;
+    this.allPinnedAchievements.length < 5 ? this.pinnedAchievements = this.allPinnedAchievements : this.pinnedAchievements = new Array(5);
+    for(var i = 0; i < this.pinnedAchievements.length; i++){
+      this.pinnedAchievements[i]=this.allPinnedAchievements[i];
+    }
 
-  constructor() { }
+    this.portalAchievements = this.achievementService.getAchievementsByGame("portal");
+    this.haloAchievements = this.achievementService.getAchievementsByGame("halo 3");
+    
+  }
 
   ngOnInit() {
   }
